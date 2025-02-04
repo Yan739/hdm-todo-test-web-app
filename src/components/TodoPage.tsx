5fec781e17497fcecee378def438ec974b1d24bf
Yann Cabrel NGATEU TAGNE <ngateuyann8@gmail.com>
2025-02-04 23:52:05 +0100

    end of frontend updates

--- src/components/TodoPage.tsx
+++ src/components/TodoPage.tsx
@@ -10,15 +10,45 @@ import { Task } from '../index';
 const TodoPage = () => {
   const api = useFetch();
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [taskName, setTaskName] = useState<string>('');
+  const [currentTask, setCurrentTask] = useState<Task | null>(null);
+  const [newTaskName, setNewTaskName] = useState<string>('');
 
   const handleFetchTasks = async () => setTasks(await api.get('/tasks'));
 
   const handleDelete = async (id: number) => {
-    // @todo IMPLEMENT HERE : DELETE THE TASK & REFRESH ALL THE TASKS, DON'T FORGET TO ATTACH THE FUNCTION TO THE APPROPRIATE BUTTON
+    await api.delete(`/tasks/${id}`);
+    handleFetchTasks();
   }
 
   const handleSave = async () => {
-    // @todo IMPLEMENT HERE : SAVE THE TASK & REFRESH ALL THE TASKS, DON'T FORGET TO ATTACH THE FUNCTION TO THE APPROPRIATE BUTTON
+    if (currentTask && taskName && taskName !== currentTask.name) {
+      // Sauvegarde en cas de modif
+      await api.patch(`/tasks/${currentTask.id}`, { name: taskName });
+      handleFetchTasks(); 
+      setCurrentTask(null); 
+      setTaskName(''); 
+    }  
+  }
+
+  const handleEdit = (task: Task) => {
+    if (currentTask?.id === task.id) {
+      // annulation de l'édition en cas de double click
+      setCurrentTask(null);
+      setTaskName("");
+    } else {
+      setCurrentTask(task);
+      setTaskName(task.name);
+    }
+  };
+
+  const handleAddTask = async () => {
+    if (newTaskName.trim()) {
+      // Ajouter la nouvelle tâche
+      await api.post('/tasks', { name: newTaskName });
+      setNewTaskName(''); 
+      handleFetchTasks(); 
+    }
   }
 
   useEffect(() => {
@@ -34,24 +64,49 @@ const TodoPage = () => {
       </Box>
 
       <Box justifyContent="center" mt={5} flexDirection="column">
-        {
-          tasks.map((task) => (
-            <Box display="flex" justifyContent="center" alignItems="center" mt={2} gap={1} width="100%">
-              <TextField size="small" value={task.name} fullWidth sx={{ maxWidth: 350 }} />
-              <Box>
-                <IconButton color="success" disabled>
-                  <Check />
-                </IconButton>
-                <IconButton color="error" onClick={() => {}}>
-                  <Delete />
-                </IconButton>
-              </Box>
+        {tasks.map((task) => (
+          <Box display="flex" justifyContent="center" alignItems="center" mt={2} gap={1} width="100%" key={task.id}>
+            <TextField
+              size="small"
+              value={currentTask?.id === task.id ? taskName : task.name}  
+              fullWidth
+              sx={{ maxWidth: 350 }}
+              onChange={(e) => setTaskName(e.target.value)}  
+              onFocus={() => handleEdit(task)}  
+              InputProps={{
+                readOnly: currentTask?.id !== task.id,  
+              }}
+            />
+
+            <Box>
+              
+              <IconButton
+                color="success"
+                disabled={currentTask?.id !== task.id || taskName.trim() === "" || taskName === currentTask.name}  
+                onClick={handleSave}
+              >
+                <Check />
+              </IconButton>
+
+              <IconButton color="error" onClick={() => handleDelete(task.id)}>
+                <Delete />
+              </IconButton>
             </Box>
-          ))
-        }
+          </Box>
+        ))}
 
         <Box display="flex" justifyContent="center" alignItems="center" mt={2}>
-          <Button variant="outlined" onClick={() => {}}>Ajouter une tâche</Button>
+          <TextField
+            size="small"
+            value={newTaskName}
+            onChange={(e) => setNewTaskName(e.target.value)}
+            placeholder="Ajouter une nouvelle tâche"
+            fullWidth
+            sx={{ maxWidth: 350 }}
+          />
+          <Button variant="outlined" onClick={handleAddTask} sx={{ marginLeft: 2 }}>
+            Ajouter une tâche
+          </Button>
         </Box>
       </Box>
     </Container>
